025f2010c0d7ee11ea0684da513ccc5502abfda4
BabarMumtaz <bbrmmtz@gmail.com>
2024-11-15 10:40:34 +0500

    updated SubmitMRN test file

--- src/test/java/com/LilyCargo/TestCases/SubmitMrnUsingUploadFileTest.java
+++ src/test/java/com/LilyCargo/TestCases/SubmitMrnUsingUploadFileTest.java
@@ -2,13 +2,19 @@ package com.LilyCargo.TestCases;
 
 import com.LilyCargo.Base.TestBaseClass;
 import com.LilyCargo.Pages.*;
+import com.LilyCargo.Util.ScreenShotUtil;
 import com.github.javafaker.Faker;
+import io.qameta.allure.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.util.Locale;
 
 public class SubmitMrnUsingUploadFileTest extends TestBaseClass {
@@ -33,11 +39,65 @@ public class SubmitMrnUsingUploadFileTest extends TestBaseClass {
 
         faker = new Faker();
         js = (JavascriptExecutor) driver;
-        log = LogManager.getLogger(FreightEditFromDetailPageTest.class);
+        log = LogManager.getLogger(SubmitMrnUsingUploadFileTest.class);
         log.info("Test setup completed.");
 
         loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
         log.info("Entered Valid Username and Password.");
     }
+    @Test(priority = 1, description = "Verify that a user can Submit MRN by uploading Doc MRN file successfully", groups = {"regression"})
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("Verify that a user can Submit MRN by uploading Doc MRN file successfully from the Freight Detail page")
+    @Epic("EP001")
+    @Feature("Feature:002")
+    @Story("As a user, I should be able to Submit MRN by uploading Doc MRN file successfully")
+    @Step("Hit Site Url -> Login with valid credentials -> Booked Freight > Freight Detail Page > Submit MRN")
+    public void SubmitMrnUsingUploadFile() throws InterruptedException {
 
+        // Check if login is successful
+        Assert.assertTrue(loginPage.isLoginSuccessful(), "Login was not successful.");
+        log.info("Login successful.");
+
+        freightListing.hoverOn1stRowClient();
+        log.info("Hover over 1st Row");
+
+        freightListing.clickOnViewFreightIcon();
+        log.info("Hover over View Icon and click");
+
+        // Switch to the new tab
+        freightListing.switchToNewTab();
+        log.info("Switched to the new tab");
+
+        // Check if the edit wrapper is displayed
+        Assert.assertTrue(freightDetail.isEditFreightIconDisplayed(), "Edit wrapper not displayed.");
+
+        // Clicked on Booked Freight Edit Icon On Detail Page
+        freightDetail.clickOnEditFreightIconDP();
+        log.info("Clicked on Booked Freight Edit Icon");
+
+        bookedFreights.clickSaveNextFreightBtn();
+        log.info("Clicked Save & Next Freight Button");
+
+        bookedFreights.enterBondedLocation("Kerkstraat 132, 1311 13 Amsterdam, Netherlands");
+        log.info("Entered Bonded Location");
+
+        bookedFreights.clickSaveReturnFreightBtn();
+        log.info("Clicked Save & Return Freight Button");
+
+        freightDetail.clickOnAlertPopupDP();
+        log.info("Clicked Cross icon of Alert");
+
+        // Log out after the test
+        loginPage.logout();
+        log.info("Logged out successfully.");
+    }
+
+    @AfterMethod
+    public void tearDown() throws IOException {
+        ScreenShotUtil.takeScreenshotAtEndOfTest(driver, "SubmitMrnUsingUploadFile");
+        if (driver != null) {
+            driver.quit(); // Closes the browser instance after each test method
+            log.info("Browser closed successfully.");
+        }
+    }
 }
