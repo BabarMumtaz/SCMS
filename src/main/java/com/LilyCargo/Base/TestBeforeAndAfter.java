36f340d9c51a40ccb3ba7d5b2315bebc8234f63d
BabarMumtaz <bbrmmtz@gmail.com>
2025-02-21 23:56:04 +0500

    Created TestBeforeAndAfter file 21/02/25 11:56 PM

---
+++ src/main/java/com/LilyCargo/Base/TestBeforeAndAfter.java
@@ -0,0 +1,56 @@
+package com.LilyCargo.Base;
+
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.LilyCargo.Util.ScreenShotUtil;
+
+public class TestBeforeAndAfter extends TestBaseClass {
+    private static final Logger log = LogManager.getLogger(TestBeforeAndAfter.class);
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        log.info("Browser initialized successfully.");
+        pageObjectManager = new PageObjectManager(driver);
+    }
+
+    public void performLogin() {
+        pageObjectManager.getLoginPage().login(prop.getProperty("username"), prop.getProperty("password"));
+        log.info("Entered valid username and password.");
+
+        // Check if login is successful
+        Assert.assertTrue(pageObjectManager.getLoginPage().isLoginSuccessful(), "Login was not successful.");
+        log.info("Login successful.");
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        String testName = result.getMethod().getMethodName();
+
+        if (result.getStatus() == ITestResult.FAILURE) {
+            try {
+                ScreenShotUtil.takeScreenshotAtEndOfTest(driver, testName);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            log.error("Test FAILED: {} - Screenshot captured.", testName);
+        } else {
+            log.info("Test {}: {}", testName, (result.getStatus() == ITestResult.SUCCESS) ? "PASSED" : "SKIPPED");
+        }
+
+        if (driver != null) {
+            driver.quit();
+            log.info("Browser closed.");
+        }
+    }
+
+}
