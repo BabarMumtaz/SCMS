d1b00df1e477c1458d3e02709a534c7be297ec1b
BabarMumtaz <bbrmmtz@gmail.com>
2025-02-25 00:24:57 +0500

    updated freight edit files 25/02/25 12:25 AM

--- src/test/java/com/LilyCargo/TestCases/FreightEditFromDetailPageTest.java
+++ src/test/java/com/LilyCargo/TestCases/FreightEditFromDetailPageTest.java
@@ -1,43 +1,15 @@
 package com.LilyCargo.TestCases;
 
-import java.io.IOException;
-
-import com.LilyCargo.Util.ScreenShotUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import com.LilyCargo.Base.TestBaseClass;
-import com.LilyCargo.Pages.FreightTestPage;
-import com.LilyCargo.Pages.FreightDetailTestPage;
-import com.LilyCargo.Pages.FreightListingTestPage;
-import com.LilyCargo.Pages.LoginTestPage;
-import com.LilyCargo.Pages.MenuBarTestPage;
+import com.LilyCargo.Base.TestBeforeAndAfter;
 import io.qameta.allure.*;
 
-public class FreightEditFromDetailPageTest extends TestBaseClass {
+public class FreightEditFromDetailPageTest extends TestBeforeAndAfter {
 
 	private Logger log;
-
-	@BeforeMethod
-	public void setup() {
-		initialization();
-		log = LogManager.getLogger(FreightEditFromDetailPageTest.class);
-		log.info("Test setup completed.");
-		performLogin();
-	}
-
-	private void performLogin() {
-
-		log.info("Attempting to log in with username: " + prop.getProperty("username"));
-		loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
-		Assert.assertTrue(loginPage.isLoginSuccessful(), "Login was not successful.");
-		log.info("Login successful.");
-	}
 
 	@Test(priority = 1, description = "Verify that a user can edit freight successfully", groups = {"regression"})
 	@Severity(SeverityLevel.BLOCKER)
@@ -48,53 +20,37 @@ public class FreightEditFromDetailPageTest extends TestBaseClass {
 	@Step("Hit Site Url -> Login with valid credentials -> Edit freight")
 	public void EditFreightTestFromDetailPage() throws InterruptedException {
 
-		Assert.assertTrue(loginPage.isLoginSuccessful(), "Login was not successful.");
-		log.info("Login successful.");
+		log = LogManager.getLogger(FreightEditFromDetailPageTest.class);
+		log.info("Starting Freight Edit Test from Detail Page.");
 
-		freightListing.hoverOn1stRowClient();
+		pageObjectManager.getFreightListing().hoverOn1stRowClient();
 		log.info("Hover over 1st Row");
 
-		freightListing.clickOnFreightID();
+		pageObjectManager.getFreightListing().clickOnFreightID();
 		log.info("Clicked on the 1st row FreightID.");
 
-		freightListing.switchToNewTab();
+		pageObjectManager.getFreightListing().switchToNewTab();
 		log.info("Switched to the new tab");
 
-		Assert.assertTrue(freightDetail.isEditFreightIconDisplayed(), "Edit wrapper not displayed.");
-		freightDetail.clickOnEditFreightIconDP();
+		Assert.assertTrue(pageObjectManager.getFreightDetail().isEditFreightIconDisplayed(), "Edit wrapper not displayed.");
+		pageObjectManager.getFreightDetail().clickOnEditFreightIconDP();
 		log.info("Clicked on Booked Freight Edit Icon");
 
-		bookedFreights.clickSaveNextFreightBtn();
+		pageObjectManager.getBookedFreights().clickSaveNextFreightBtn();
 		log.info("Clicked Save & Next Freight Button");
 
-		bookedFreights.enterBondedLocation("Kerkstraat 132, 1311 13 Amsterdam, Netherlands");
+		pageObjectManager.getBookedFreights().enterBondedLocation("Kerkstraat 132, 1311 13 Amsterdam, Netherlands");
 		log.info("Entered Bonded Location");
 
-		bookedFreights.clickSaveReturnFreightBtn();
+		pageObjectManager.getBookedFreights().clickSaveReturnFreightBtn();
 		log.info("Clicked Save & Return Freight Button");
 
-		freightDetail.clickOnAlertPopupDP();
+		pageObjectManager.getFreightDetail().clickOnAlertPopupDP();
 		log.info("Clicked Cross icon of Alert");
 
-		loginPage.logout();
+		// Log out after the test
+		pageObjectManager.getLoginPage().logout();
 		log.info("Logged out successfully.");
 	}
 
-	@AfterMethod
-	public void tearDown() {
-		captureScreenshot("EditFreightTestFromDetailPage");
-		if (driver != null) {
-			driver.quit();
-			log.info("Browser closed successfully.");
-		}
-	}
-
-	private void captureScreenshot(String testName) {
-		try {
-			ScreenShotUtil.takeScreenshotAtEndOfTest(driver, testName);
-			log.info("Screenshot captured for test: " + testName);
-		} catch (IOException e) {
-			log.error("Error capturing screenshot.", e);
-		}
-	}
 }
