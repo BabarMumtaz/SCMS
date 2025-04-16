2c3bc7ffd86a0ed7c5b0db68805f955bfac4f90d
Babar, Mumtaz <babar.sqa.mateebtech@gmail.com>
2025-04-16 20:14:22 +0500

    Updated Test files 16 April 25, 8:14 PM

--- src/test/java/com/LilyCargo/TestCases/ClientAddTest.java
+++ src/test/java/com/LilyCargo/TestCases/ClientAddTest.java
@@ -1,24 +1,13 @@
 package com.LilyCargo.TestCases;
 
-import java.io.IOException;
-import java.time.Duration;
-
-import com.LilyCargo.Pages.*;
-import com.LilyCargo.Util.ScreenShotUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.LilyCargo.Base.TestBaseClass;
 import com.LilyCargo.Util.ExcelUtil;
-import com.github.javafaker.Faker;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
@@ -31,21 +20,6 @@ import io.qameta.allure.Story;
 public class ClientAddTest extends TestBaseClass {
 
     Logger log;
-
-    @BeforeMethod
-    public void setup() {
-        initialization(); // Opens a new browser instance
-
-        log = LogManager.getLogger(ClientAddTest.class);
-        log.info("Test setup completed.");
-
-        performLogin();
-    }
-
-    private void performLogin() {
-        loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
-        log.info("Entered valid username and password.");
-    }
 
     // DataProvider to fetch data from Excel file
     @DataProvider
@@ -67,54 +41,35 @@ public class ClientAddTest extends TestBaseClass {
     public void AddClientTest(String Name, String ContactPerson, String Address1, String Email1, String FinancialEmail,
                               String FiscalMattersEmail1, String CeoEmail1, String TelephoneNumber1, String RegionDropDown,
                               String CountryDropDown, String ZipCity, String Vat, String LfrDropDown) {
-        Assert.assertTrue(loginPage.isLoginSuccessful(), "Login was not successful.");
-        log.info("Login successful.");
 
-        menuBar.clickFreightRelationsMenu();
+        log = LogManager.getLogger(ClientAddTest.class);
+        log.info("Starting Client Add Test from Freight Relations.");
+
+        pageObjectManager.getMenuBar().clickFreightRelationsMenu();
         log.info("Clicked Freight Relations Menu");
 
-        menuBar.clickClientFRSubMenu();
+        pageObjectManager.getMenuBar().clickClientFRSubMenu();
         log.info("Clicked Client FR Sub Menu");
 
-        Assert.assertTrue(clientPage.isHeadingDisplayed(), "Heading Not Displayed");
-        log.info("Heading: " + clientPage.getPageHeading());
+        Assert.assertTrue(pageObjectManager.getClientPage().isHeadingDisplayed(), "Heading Not Displayed");
+        log.info("Heading: " + pageObjectManager.getClientPage().getPageHeading());
 
-        clientPage.clickAddClientBtn();
+        pageObjectManager.getClientPage().clickAddClientBtn();
         log.info("Clicked Client Add button");
 
         // Log the dropdown selections for clarity
         log.info("Adding client data: Region - " + RegionDropDown + ", Country - " + CountryDropDown + ", LFR - "
                 + LfrDropDown);
 
-        clientPage.addClientData(Name, ContactPerson, Address1, Email1, FinancialEmail, FiscalMattersEmail1, CeoEmail1,
+        pageObjectManager.getClientPage().addClientData(Name, ContactPerson, Address1, Email1, FinancialEmail, FiscalMattersEmail1, CeoEmail1,
                 TelephoneNumber1, RegionDropDown, CountryDropDown, ZipCity, Vat, LfrDropDown);
 
-        clientPage.clickOnAlertPopupLP();
+        pageObjectManager.getClientPage().clickOnAlertPopupLP();
         log.info("Clicked Cross icon of Alert");
 
-        Assert.assertTrue(clientPage.isHeadingDisplayed(), "Heading Not Displayed");
-        loginPage.logout();
-    }
+        Assert.assertTrue(pageObjectManager.getClientPage().isHeadingDisplayed(), "Heading Not Displayed");
 
-    @AfterMethod
-    public void tearDown() {
-        captureScreenshot("AddClientTest");
-        closeBrowser();
-    }
-
-    private void captureScreenshot(String testName) {
-        try {
-            ScreenShotUtil.takeScreenshotAtEndOfTest(driver, testName);
-            log.info("Screenshot captured for test: " + testName);
-        } catch (IOException e) {
-            log.error("Error capturing screenshot.", e);
-        }
-    }
-
-    private void closeBrowser() {
-        if (driver != null) {
-            driver.quit();
-            log.info("Browser closed successfully.");
-        }
+        pageObjectManager.getLoginPage().logout();
+        log.info("Logged out successfully.");
     }
 }
