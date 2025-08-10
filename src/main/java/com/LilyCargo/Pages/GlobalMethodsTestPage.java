9ad7c6148e9f742b245d30fb441521409ac5c96a
BabarMumtaz <bbrmmtz@gmail.com>
2025-08-10 16:26:16 +0500

    updated global and warehouse test classes

--- src/main/java/com/LilyCargo/Pages/GlobalMethodsTestPage.java
+++ src/main/java/com/LilyCargo/Pages/GlobalMethodsTestPage.java
@@ -1,9 +1,6 @@
 package com.LilyCargo.Pages;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -17,7 +14,6 @@ public class GlobalMethodsTestPage {
 
     WebDriver driver;
     JavascriptExecutor executor;
-    Actions actions;
     WebDriverWait wait;
 
     // Constructor that will be automatically called as soon as the object of the class is created
@@ -26,7 +22,6 @@ public class GlobalMethodsTestPage {
         this.executor = (JavascriptExecutor) this.driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         PageFactory.initElements(driver, this);
-        this.actions = new Actions(driver);
     }
 
     /** ---------- Locators ---------- */
@@ -53,6 +48,47 @@ public class GlobalMethodsTestPage {
     WebElement alertPopupListingPage;
 
     /** ---------- Methods ---------- */
+
+    // ===== 2. Generic Dynamic Heading Method =====
+    /**
+     * Get the heading text for a given page.
+     * Supports both h2 and h5 tag headings.
+     * @param headingText Visible heading text on the page.
+     * @return The heading WebElement.
+     */
+    private WebElement getPageHeading(String headingText) {
+        String headingXpath = String.format("//h2[text()='%s'] | //h5[text()='%s']", headingText, headingText);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(headingXpath)));
+    }
+
+    /**
+     * Get the heading text for verification.
+     * @param headingText The expected heading text.
+     * @return The actual heading text.
+     */
+    public String getPageHeadingText(String headingText) {
+        return getPageHeading(headingText).getText();
+    }
+
+    // ===== 2. Generic Success Alert Method ===== Finds a success alert message containing the given text.
+    public WebElement getSuccessAlertElement(String alertText) {
+        String alertXpath = String.format("//div[contains(text(),'%s')]", alertText);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(alertXpath)));
+    }
+
+    //Gets the text of a success alert.
+    public String getSuccessAlertText(String alertText) {
+        return getSuccessAlertElement(alertText).getText();
+    }
+
+     // Checks if a success alert is displayed.
+    public boolean isSuccessAlertDisplayed(String alertText) {
+        try {
+            return getSuccessAlertElement(alertText).isDisplayed();
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
 
     public void selectDropdownValue(WebElement dropdown, WebElement dropdownValue) {
         dropdown.click();
